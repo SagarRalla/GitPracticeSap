7cf041af00a19d2d8b5d4313b62fe96ccb2bd9ba
RSagar235 <124336584+RSagar235@users.noreply.github.com>
2023-09-25 23:04:22 +0530

    super1

---
+++ src/test/java/com/mt/GitPracticeSap.java
@@ -0,0 +1,10 @@
+package com.mt;
+import org.testng.annotations.Test;
+
+public class GitPracticeSap {
+
+	@Test
+	public static void main(String[] args) {
+		System.out.println("git practice SAP");
+	}
+}
